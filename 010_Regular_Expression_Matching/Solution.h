38562e4db1600d578cfe4b9fd0c220b0bb8712aa
Chao Li <lichaorodxx@gmail.com>
2015-06-12 00:33:11 -0700

    finished to problem 10

---
+++ 010_Regular_Expression_Matching/Solution.h
@@ -0,0 +1,23 @@
+//
+//  Solution.h
+//  010_Regular_Expression_Matching
+//
+//  Created by Chao Li on 6/12/15.
+//  Copyright (c) 2015 Chao Li. All rights reserved.
+//
+
+#ifndef ___10_Regular_Expression_Matching__Solution__
+#define ___10_Regular_Expression_Matching__Solution__
+
+#include <stdio.h>
+#include <string>
+#include <vector>
+using namespace std;
+
+class Solution
+{
+public:
+	bool isMatch(string s, string p);
+};
+
+#endif /* defined(___10_Regular_Expression_Matching__Solution__) */
